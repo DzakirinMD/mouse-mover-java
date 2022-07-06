d3fcb43600a780b990dd1f908d2dea5ce1d0e619
Dzakirin <m.abdrahim@dxc.com>
2022-07-06 15:59:32 +0800

    Added control on how many time cursor should move

--- MouseMover.java
+++ MouseMover.java
@@ -1,6 +1,18 @@
 import java.awt.*;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * Datatime Formatter: https://www.javatpoint.com/java-localdatetime#:~:text=Java%20LocalDateTime%20class%20is%20an,%2DHH%2Dmm%2Dss.
+ * How to calculate time : https://www.callicoder.com/how-to-add-subtract-days-hours-minutes-seconds-to-date-java/
+ */
 public class MouseMover {
 
     public static void main(String[] args) {
@@ -8,21 +20,27 @@ public class MouseMover {
         System.out.println("=====================================\n" +
                 "Welcome to mouse mover program !!! \n" +
                 "=====================================\n" +
-                "The cursor/mouse will move in every 10 seconds.\n" +
-                "Hence if you want it to move for 1 minutes please enter 6 " +
-                "as the number of time the cursor should move \n\n" +
-                "6 times = 1 minute \n" +
-                "60 times = 10 minute \n" +
-                "360 times = 1 hour \n" +
-                "720 times  = 2 hour");
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("How many time should the cursor move : ");
-        int times = myObj.nextInt();  // Read user input
+                "The cursor/mouse will move in every 10 seconds.\n");
 
-        new MouseMover().changeCursorLocation(times);
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please enter how many minutes (int) should the cursor move : ");
+        int minutes = myObj.nextInt();  // Read user input
+
+        new MouseMover().changeCursorLocation(minutes);
     }
 
-    public synchronized void changeCursorLocation(int times){
+    public synchronized void changeCursorLocation(int minutes){
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime time = LocalTime.parse(LocalTime.now().format(format));
+        System.out.println("Current time is : " + time);
+
+        // Add hours, minutes, or seconds
+        LocalTime newTime = time.plusMinutes(minutes);
+        System.out.println("The time when program will end is : " + newTime);
+
+        int returnVal = time.compareTo(newTime);
+
         try {
             Robot robot = new Robot();
             // width and height of the screen
@@ -31,13 +49,31 @@ public class MouseMover {
             int movement = 0;
             long counter = 0;
 
-            while (counter <= times){
+            while (time.compareTo(newTime) < 0){
+
+                time = LocalTime.parse(LocalTime.now().format(format));
+                if (returnVal > 0) {
+                    System.out.println("Current time while program is running : " + time);
+                }else if (returnVal == 0) {
+                    System.out.println("Current time while program is running : " + time);
+                }else {
+                    System.out.println("Current time while program is running : " + time);
+                }
+
                 //mouse will move in every 10 seconds
                 this.wait(10000);
-                counter++;
-                System.out.println("Cursor has moved " + (movement = movement + 1) + " times already !");
+//                counter++;
+//                System.out.println("Cursor has moved " + (movement = movement + 1) + " times already !");
                 robot.mouseMove((int)(Math.random()*width), (int)(Math.random()*height));
             }
+
+//            while (counter <= times){
+//                //mouse will move in every 10 seconds
+//                this.wait(10000);
+//                counter++;
+//                System.out.println("Cursor has moved " + (movement = movement + 1) + " times already !");
+//                robot.mouseMove((int)(Math.random()*width), (int)(Math.random()*height));
+//            }
         } catch (AWTException | InterruptedException e) {
             e.printStackTrace();
         }
